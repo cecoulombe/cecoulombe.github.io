b3e79ac352fe2de2fe8297a4c0050c3bfb91c02b
Caitlin Coulombe <ccoulomb@student.ubc.ca>
2024-09-24 20:31:03 -0700

    Trying again

---
+++ tracker_React/src/strings.js
@@ -0,0 +1,20 @@
+const strings = {
+    pageTitle: "Net Worth Calculator",
+    currencyLabel: "Choose a currency: ",
+    cad: 'Canadian Dollar (CAD)',
+    usd: 'American Dollar (USD)',
+    eur: 'Euro (EUR)',
+    try: 'Turkish Lira (TRY)',
+    assetTitle: "Assets",
+    liabilityTitle: "Liabilities", 
+    amountTitle: "Amount",
+    addAssetButton: "Add asset",
+    addLiabilityButton: "Add liability",
+    assetsDescription: "Assets may include: saved money, investments, real estate, &c.",
+    liabilitiesDescription: "Liabilities may include: debt, loans, mortgage, &c.",
+    assetsSum: "Sum of Assets: ",
+    liabilitiesSum: "Sum of Liabilities: " ,
+    netWorth: "Net worth: ",
+}
+
+export default strings;
